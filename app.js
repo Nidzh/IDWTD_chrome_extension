04dc4e1bd2eaba4bcc49edffc5e9ffe26bbed141
Akshin Ragimov <akshinragimov@gmail.com>
2021-10-01 12:23:08 +0300

    upd

--- app.js
+++ app.js
@@ -10,6 +10,11 @@ let currentDataProperty = map[currentOrigin];
 let currentData = IDWTD[currentDataProperty];
 
 function mark(elementToMark) {
+    let elChildren = elementToMark.children;
+    let imageExists = Array.prototype.some.call(elChildren, el => {
+        return el.className === 'underpic';
+    })
+    if (imageExists) return;
     elementToMark.insertAdjacentHTML('afterBegin', '<img src="http://s1.iconbird.com/ico/2014/1/567/w512h5121389807754burnicon.png" class="underpic" style="position: absolute; width: 16%; z-index: 1; right: 7%; top: 60%;" />');
 }
 
@@ -36,4 +41,9 @@ function parse(currentData) {
     })
 }
 
-parse(currentData)
+parse(currentData);
+
+let interval = 1000;
+setInterval(() => {
+    parse(currentData)
+}, interval);
